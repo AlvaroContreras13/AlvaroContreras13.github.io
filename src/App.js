62a2da9c67c6cf0929699f72ea5221f9639528c7
BCZLopezCatunta <bl2020068946@virtual.upt.pe>
2024-09-14 12:22:56 -0500

    login de google

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Login /> {/* muestra el componente Login */}
+      <LoginGoogle /> {/* muestra el componente Login */}
     </div>
   );
 }
